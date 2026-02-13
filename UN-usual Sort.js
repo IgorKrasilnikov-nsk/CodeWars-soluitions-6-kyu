e2f383e408a41b5c627ff7bd13ef219b8bb184e8
Igor Krasilnikov <kite84nsk@gmail.com>
2026-02-13 09:30:45 +0700

    Create UN-usual Sort.js

---
+++ UN-usual Sort.js
@@ -0,0 +1,48 @@
+/*
+In another Kata I came across a weird sort function to implement. 
+We had to sort characters as usual ( 'A' before 'Z' and 'Z' before 'a' ) except that the numbers had to be sorted after the letters ( '0' after 'z') !!!
+(After a couple of hours trying to solve this unusual-sorting-kata I discovered final tests used **usual** sort (digits **before** letters :-)
+So, the function you'll have to code will return a sorted letters as usual, but will put digits (or one-digit-long numbers ) after letters.
+
+Examples
+["a","z","b"] --> ["a","b","z"]  // as usual
+["a","Z","B"] --> ["B","Z","a"]  // as usual
+
+// ... but ...
+["1","z","a"] --> ["a","z","1"]
+["1","Z","a"] --> ["Z","a","1"]
+[3,2,1"a","z","b"] --> ["a","b","z",1,2,3]
+[3,"2",1,"a","c","b"] --> ["a","b","c",1,"2",3]
+Note: digits will be sorted after "same-digit-numbers", eg: 1 is before "1", "2" after 2.
+
+[3,"2",1,"1","3",2] --> [1,"1",2,"2",3,"3"]
+You may assume that argument will always be an array/list of characters or one-digit-long numbers.
+*/
+function unusualSort(arrayOfChars){
+  return arrayOfChars.sort((a, b) => {
+    const aIsDigit = !isNaN(a) && a !== ' ';
+    const bIsDigit = !isNaN(b) && b !== ' ';
+        
+    if (!aIsDigit && bIsDigit) return -1;
+    if (aIsDigit && !bIsDigit) return 1;
+          
+    if (!aIsDigit && !bIsDigit) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    }
+        
+    const numA = Number(a);
+    const numB = Number(b);
+        
+    if (numA !== numB) {
+      return numA - numB;
+    } else {
+      if (typeof a === 'number' && typeof b === 'string') return -1;
+      if (typeof a === 'string' && typeof b === 'number') return 1;
+      return 0;
+    }
+  });
+}
+console.log(unusualSort([3,"3","2",2,"2","1",1,"a","b","c"])); // ["a","b","c",1,"1",2,"2","2",3,"3"]
+console.log(unusualSort(['c', 'b', 'a', '9', '5', '0', 'X', 'Y', 'Z'])); // ['X', 'Y', 'Z', 'a', 'b', 'c', '0', '5', '9']
